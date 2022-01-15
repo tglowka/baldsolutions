ca86cee7da720ecc817ed35b4922c1921266c24f
tglowka <38429856+tglowka@users.noreply.github.com>
2022-01-15 10:37:36 +0100

    Dotnet/nsubstitute 1 (#2)
    * Refactor solution structure (one solution, many projects). Add nsubstitute_1 project with its tests.
    
    * Renaming refactoring.
    
    Co-authored-by: Tomasz Glowka <tomek3396@gmail.com>

---
+++ .NET/nsubstitute_1/CustomClass.cs
@@ -0,0 +1,22 @@
+﻿namespace nsubstitute_1
+{
+    public class CustomClass
+    {
+        private readonly ICustomService _customService;
+
+        public CustomClass(ICustomService customService)
+        {
+            _customService = customService;
+        }
+
+        public void RunProcess(int param_1, int param_2, int param_3)
+        {
+            _customService.Run(
+                descriptorBuilder => descriptorBuilder
+                    .BuildMyProperty_1(param_1)
+                    .BuildMyProperty_2(param_2)
+                    .BuildMyProperty_3(param_3)
+            );
+        }
+    }
+}
---
+++ .NET/nsubstitute_1/IDescriptor.cs
@@ -0,0 +1,9 @@
+﻿namespace nsubstitute_1
+{
+    public interface IDescriptor
+    {
+        public IDescriptor BuildMyProperty_1(int myProperty_1);
+        public IDescriptor BuildMyProperty_2(int myProperty_2);
+        public IDescriptor BuildMyProperty_3(int myProperty_3);
+    }
+}
