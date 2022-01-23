474e62eac8a9d1c23b6d9c2a290314cf645bd62b
Tomasz Glowka <tomek3396@gmail.com>
2022-01-23 14:09:49 +0100

    Add compare-objects project and tests.

---
+++ .NET/compare-objects/InternalData.cs
@@ -0,0 +1,4 @@
+﻿public class InternalData
+{
+    public InternalData() { }
+}
---
+++ .NET/compare-objects/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace compare_objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var equalsOperationCount = 1_000_000_000;
+
+            var obj = new InternalData();
+            var particlesWithEquals = (new ParticleWithEquals(1, obj), new ParticleWithEquals(1, obj));
+            var particlesWithoutEquals = (new ParticleWithoutEquals(1, obj), new ParticleWithoutEquals(1, obj));
+
+            var diagnostic = new Diagnostic(particlesWithEquals, particlesWithoutEquals, equalsOperationCount);
+            diagnostic.MeasureExecutionTimeWithEquals();
+            diagnostic.MeasureExecutionTimeWithoutEquals();
+        }
+    }
+}
