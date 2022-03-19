1788147ef17bc00f839f54522932b0b7a5979a66
tglowka <38429856+tglowka@users.noreply.github.com>
2022-03-20 00:40:16 +0100

    Add closure project and its tests. (#12)
    Co-authored-by: Tomasz Glowka <tomek3396@gmail.com>

---
+++ .NET/closure/Closure.cs
@@ -0,0 +1,28 @@
+﻿public class Closure
+{
+    public static Func<int> GetFuncWithClosure(int arg)
+    {
+        int internalVariable = arg;
+
+        var func = new Func<int>(() =>
+        {
+            internalVariable -= 5;
+
+            return 10 / internalVariable;
+        });
+
+        return func;
+    }
+
+    public static Func<int, int> GetFuncWithoutClosure()
+    {
+        var func = new Func<int, int>((arg) =>
+        {
+            arg -= 5;
+
+            return 10 / arg;
+        });
+
+        return func;
+    }
+}
