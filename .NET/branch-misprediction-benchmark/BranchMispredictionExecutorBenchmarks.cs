24f111a40787515d13351b6cbdefdb4add6a073f
Tomasz Glowka <tomek3396@gmail.com>
2022-03-27 00:12:20 +0100

    Adds branch-misprediction project and benchmark.

---
+++ .NET/branch-misprediction-benchmark/BranchMispredictionExecutorBenchmarks.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnosers;
+
+[HardwareCounters(HardwareCounter.BranchMispredictions, HardwareCounter.BranchInstructions)]
+public class BranchMispredictionExecutorBenchmarks
+{
+    private readonly BranchMispredictionExecutor _executor;
+    private readonly int _size = 100000;
+
+    public BranchMispredictionExecutorBenchmarks()
+    {
+        _executor = new BranchMispredictionExecutor(_size);
+    }
+
+    [Benchmark]
+    public void SortedSumLessThan()
+    {
+        var sum = _executor.SortedSumLessThan(_size / 2);
+    }
+
+    [Benchmark]
+    public void UnsortedSumLessThan()
+    {
+        var sum = _executor.UnsortedSumLessThan(_size / 2);
+    }
+}
