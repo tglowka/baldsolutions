836ff154dd64407233279996a0eec28222683486
Tomasz Glowka <tomek3396@gmail.com>
2022-04-24 16:07:29 +0200

    Add arraypool project and benchmark.

---
+++ .NET/arraypool-benchmark/ArrayExecutorBenchmark.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Attributes;
+
+[MemoryDiagnoser]
+public class ArrayExecutorBenchmark
+{
+    private ArrayExecutor _arrayExecutor;
+
+    [Params(10_000, 100_000, 1_000_000)]
+    public int size;
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        _arrayExecutor = new ArrayExecutor(size);
+    }
+
+    [Benchmark]
+    public void ArrayLoop() => _arrayExecutor.ArrayLoop();
+
+    [Benchmark]
+    public void ArrayPoolLoop() => _arrayExecutor.ArrayPoolLoop();
+}
---
+++ .NET/arraypool/ArrayExecutor.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Buffers;
+
+public class ArrayExecutor
+{
+    private readonly int _size;
+    private readonly Random _random;
+
+    public ArrayExecutor(int size)
+    {
+        _random = new Random();
+        _size = size;
+    }
+
+    public void ArrayLoop()
+    {
+        var array = new int[_size];
+
+        for (int i = 0; i < _size; i++)
+            array[i] = _random.Next(100);
+
+        var minMax = new int[2]
+        {
+            array.Min(), array.Max()
+        };
+
+        Process(minMax);
+    }
+
+    public void ArrayPoolLoop()
+    {
+        var array = ArrayPool<int>.Shared.Rent(_size);
+
+        for (int i = 0; i < _size; i++)
+            array[i] = _random.Next(100);
+
+        var minMax = new int[2]
+        {
+            array.Min(), array.Max()
+        };
+
+        Process(minMax);
+
+        ArrayPool<int>.Shared.Return(array);
+    }
+
+    private void Process(int[] array)
+    {
+        // some computations here
+    }
+}
