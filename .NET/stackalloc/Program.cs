7cb1112bbcc5c4f867f4cbe0295322a9a05a76da
Tomasz Glowka <tomek3396@gmail.com>
2022-05-02 15:48:15 +0200

    Add stackalloc project and benchmark.

---
+++ .NET/stackalloc/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// the following kills my app - StackOverflowException
+//var size = 1_000_000;
+//var executor = new StackallocExecutor(size);
+//executor.Stackalloc();
---
+++ .NET/stackalloc/StackallocExecutor.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class StackallocExecutor
+{
+    private readonly int _size;
+    private readonly Random _random;
+
+    public StackallocExecutor(int size)
+    {
+        _size = size;
+        _random = new Random();
+    }
+
+    public int Stackalloc()
+    {
+        Span<int> span = stackalloc int[_size];
+
+        for (int i = 0; i < span.Length; i++)
+            span[i] = _random.Next();
+
+        for (int i = 0; i < span.Length; i++)
+            if (i % 2 == 1)
+                span[i] += 1;
+
+        var max = span[0];
+
+        for (int i = 1; i < span.Length; i++)
+            if (span[i] > max)
+                max = span[i];
+
+        return max;
+    }
+
+    public int NoStackalloc()
+    {
+        var array = new int[_size];
+
+        for (int i = 0; i < array.Length; i++)
+            array[i] = _random.Next();
+
+        for (int i = 0; i < array.Length; i++)
+            if (i % 2 == 1)
+                array[i] += 1;
+
+        var max = array[0];
+
+        for (int i = 1; i < array.Length; i++)
+            if (array[i] > max)
+                max = array[i];
+
+        return max;
+    }
+}
