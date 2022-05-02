8fb26f0d3f55ccd94944bef7dfd9b8a13c055877
tglowka <38429856+tglowka@users.noreply.github.com>
2022-05-02 15:49:19 +0200

    Add stackalloc project and benchmark. (#15)
    Co-authored-by: Tomasz Glowka <tomek3396@gmail.com>

---
+++ .NET/stackalloc-benchmark/StacallocExecutorBenchmarks.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Attributes;
+
+[MemoryDiagnoser]
+public class StackallocExecutorBenchmarks
+{
+    [Benchmark]
+    [Arguments(100)]
+    [Arguments(1_000)]
+    [Arguments(10_000)]
+    public void Stackalloc(int size)
+    {
+        var stacallocExecutor = new StackallocExecutor(size);
+        stacallocExecutor.Stackalloc();
+    }
+
+    [Benchmark]
+    [Arguments(100)]
+    [Arguments(1_000)]
+    [Arguments(10_000)]
+    public void NoStackalloc(int size)
+    {
+        var stacallocExecutor = new StackallocExecutor(size);
+        stacallocExecutor.NoStackalloc();
+    }
+}
