bf2c0931cfa84e2dc226b8a9e6c2fbdc37f36d0c
Tomasz Glowka <tomek3396@gmail.com>
2022-08-09 23:13:18 +0200

    Add c# and f# tail-call projects.

---
+++ .NET/tail-call-csharp/Program.cs
@@ -0,0 +1,34 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+//BenchmarkRunner.Run<RecursionBenchmark>();
+
+//Both throw StackOverflowException
+Console.WriteLine(Recursion.StandardRecursion(30000));
+Console.WriteLine(Recursion.TailRecursion(30000));
+
+public static class Recursion
+{
+    public static int TailRecursion(int x, int acc = 1) =>
+        x == 0
+            ? acc
+            : TailRecursion(x - 1, acc * x);
+
+    public static int StandardRecursion(int x) =>
+        x < 1
+            ? 1
+            : x * StandardRecursion(x - 1);
+}
+
+//[MemoryDiagnoser]
+//public class RecursionBenchmark
+//{
+//    [Params(10_000, 20_000, 30_000)]
+//    public int size;
+
+//    [Benchmark]
+//    public void StandardRecursion() => Recursion.StandardRecursion(size);
+
+//    [Benchmark]
+//    public void TailRecursion() => Recursion.TailRecursion(size);
+//}
---
+++ .NET/tail-call-fsharp/Program.fs
@@ -0,0 +1,40 @@
+﻿module tail_call 
+
+open System
+open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Running
+
+module StandardRecursion=
+    let rec factorial x =
+        match x with
+        | var1 when var1 < 1 -> 1
+        | x -> x * factorial (x-1)
+
+module TailRecursion  =    
+    let factorial x = 
+       let rec factorial2 x acc =
+            match x with
+            | 0 -> acc
+            | x -> factorial2 (x-1) (acc * x)
+       factorial2 x 1
+
+//[<MemoryDiagnoser>]
+//type Benchmarks () =
+//    [<Params(10000, 20000, 30000)>]
+//    member val public count = 0 with get, set
+
+//    [<Benchmark>]
+//    member this.StandardRecursion () = StandardRecursion.factorial this.count
+
+//    [<Benchmark>]
+//    member this.TailRecursion () = TailRecursion.factorial this.count
+
+
+[<EntryPoint>]
+let main argv =
+    //BenchmarkRunner.Run<Benchmarks>() |> ignore
+    
+    //Only StandardRecursion throws StackOverflowException
+    Console.WriteLine( StandardRecursion.factorial 6)
+    Console.WriteLine( TailRecursion.factorial 6)
+    0
