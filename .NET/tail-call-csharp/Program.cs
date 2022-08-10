10cdde680898eae99012b361b2718146f1b4b146
tglowka <38429856+tglowka@users.noreply.github.com>
2022-08-10 23:43:36 +0200

    Dotnet/tail call (#17)
    * Add c# and f# tail-call projects.
    
    * Code refactor. Add x86 and x64 target platforms.
    
    * Remove BenchmarkDotNet.
    
    Co-authored-by: Tomasz Glowka <tomek3396@gmail.com>

---
+++ .NET/tail-call-csharp/Program.cs
@@ -0,0 +1,15 @@
+﻿Recursion.StandardRecursion(6000000);
+Recursion.TailRecursion(6000000);
+
+public static class Recursion
+{
+    public static int TailRecursion(int x, int acc = 1) =>
+        x == 0
+            ? acc
+            : TailRecursion(x - 1, acc * x);
+
+    public static int StandardRecursion(int x) =>
+        x < 1
+            ? 1
+            : x * StandardRecursion(x - 1);
+}
---
+++ .NET/tail-call-fsharp/Program.fs
@@ -0,0 +1,21 @@
+﻿module tail_call 
+
+open System
+
+module StandardRecursion=
+    let rec factorial x =
+        if x < 1 then 1
+        else x * factorial (x-1)
+
+module TailRecursion  =    
+    let factorial x = 
+       let rec factorial2 x acc =
+            if x = 0 then acc
+            else factorial2 (x-1) (acc * x)
+       factorial2 x 1
+
+[<EntryPoint>]
+let main argv =
+    StandardRecursion.factorial 6000000
+    TailRecursion.factorial 6000000
+    0
