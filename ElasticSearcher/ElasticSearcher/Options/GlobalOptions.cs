cd48dc50ff67f79cf1e9f7614567975b58835ae4
Tomasz Glowka <tomek3396@gmail.com>
2023-08-28 20:06:52 +0200

    Remove Elasticsearcher dir

--- ElasticSearcher/ElasticSearcher/Options/GlobalOptions.cs
+++
@@ -1,6 +0,0 @@
-namespace ElasticSearcher.Options;
-
-public static class GlobalOptions
-{
-    public static readonly UriOption UriOption = new();
-}
--- ElasticSearcher/ElasticSearcher/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.CommandLine;
-
-namespace ElasticSearcher;
-
-class Program
-{
-    static async Task<int> Main(string[] args)
-        => await CommandFactory.CreateRootCommand().InvokeAsync(args);
-}
