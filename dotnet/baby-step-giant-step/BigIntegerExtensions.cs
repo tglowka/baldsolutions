7de236e32ef509b651baac1ddf6d08c99b47fd6b
Tomasz Glowka <tomek3396@gmail.com>
2024-06-02 13:44:59 +0200

    Add baby step giant step and brute force algorithms implementations. Add benchmark.

---
+++ dotnet/baby-step-giant-step/BigIntegerExtensions.cs
@@ -0,0 +1,80 @@
+﻿using System.Numerics;
+
+public static class BruteForceAlgorithm
+{
+    public static BigInteger Compute(BigInteger @base, BigInteger result, BigInteger mod, long limit)
+    {
+        for (var i = 0; i < limit; i++)
+        {
+            var r = BigInteger.ModPow(@base, i, mod);
+            if (r == result)
+            {
+                return i;
+            }
+        }
+        
+        throw new Exception("Not found!");
+    }    
+}
+
+public static class BabyStepGianStepAlgorithm
+{
+    public static BigInteger Compute(BigInteger @base, BigInteger result, BigInteger mod, long limit)
+    {
+        var m = (long)Math.Ceiling(Math.Sqrt(limit));
+
+        var storage = new Dictionary<BigInteger, long>();
+
+        for (var j = 0; j < m; j++)
+        {
+            var value = BigInteger.ModPow(@base, j, mod);
+            storage.TryAdd(value, j);
+        }
+
+        var baseInverse = BigInteger.ModPow(@base.ModInverse(mod), m, mod);
+        for (var i = 0; i < m; i++)
+        {
+            var value = BigInteger.Remainder(result * BigInteger.ModPow(baseInverse, i, mod), mod);
+            if (storage.TryGetValue(value, out var j))
+            {
+                return new BigInteger(i * m + j);
+            }
+        }
+
+        throw new Exception("i and j not found");
+    }
+}
+
+public static class BigIntegerExtensions
+{
+    public static BigInteger ModInverse(this BigInteger source, long mod)
+        => source.ModInverse(new BigInteger(mod));
+
+    public static BigInteger ModInverse(this BigInteger source, BigInteger mod)
+    {
+        BigInteger t = 0;
+        BigInteger r = mod;
+        BigInteger new_t = 1;
+        BigInteger new_r = source;
+
+        while (new_r != 0)
+        {
+            var quotient = BigInteger.Divide(r, new_r);
+
+            (t, new_t) = (new_t, t - quotient * new_t);
+            (r, new_r) = (new_r, r - quotient * new_r);
+        }
+
+        if (r > 1)
+        {
+            throw new Exception("a is not invertible");
+        }
+
+        if (t < 0)
+        {
+            t += mod;
+        }
+
+        return t;
+    }
+}
---
+++ dotnet/baby-step-giant-step/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Numerics;
+
+var A = BigInteger.Parse("21397");
+var M = BigInteger.Parse("5210644015679228794060694325390955853335898483908056458352183851018372555735221");
+var I = BigInteger.Parse("598156793758099511761753839885471060009647209443751599122360246998383254800634");
+var R = BigInteger.Parse("3522197356311839067406761763558521917536353254804586391157901835191894911474192");
+
+// Console.WriteLine(BigInteger.ModPow(I, A, M));
+// Console.WriteLine(BabyStepGianStepAlgorithm.Compute(I, R, M, 2139123 * 400));
+Console.WriteLine(BruteForceAlgorithm.Compute(I, R, M, 2139123 * 400));
+// Console.WriteLine(2139123 * 400);
+
+
+return;
